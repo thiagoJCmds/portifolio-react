22625a0c01c1836b5b989b9f4eda3c5d9e9ebd8f
Thiago Manoel da Silva <thiagomds03@gmail.com>
2024-02-12 15:19:55 -0300

    reponsivity

--- src/components/Sidebar/Styles.jsx
+++ src/components/Sidebar/Styles.jsx
@@ -3,33 +3,35 @@ import styled from "styled-components";
 export const Container2 = styled.div`
 display: block;
 text-align: center;
-position: sticky;  
-top: 80px;
+@media (max-width:1031px) and (orientation:landscape){
+  margin-left: 30px;
+  text-align: center;
+}
+@media (min-width:1024px){
+  position: sticky;  
+  top: 80px;
+  
+}
+@media (max-width:900px) and (orientation:landscape){
+
+
+}
+
+@media (max-width:480px){
+  display: block;
+  max-width: 100vw;
+  width: 100%;
+}  
 `
 export const Img = styled.img`
 
 max-width: 100%;
 border-radius: 50%;
 border: solid #000 2px;
-      
-
-    @media (max-width:1031px) and (orientation:landscape){
-      margin-left: 30px;
+    @media (max-width:768px){
+       max-width:200px ;
     }
-
-    @media (max-width: 768px) {
-
-      max-width: 80%;
-      width: 100%;
-      display: block;
-      img{
-        max-width:300px;
-      }
-    }
-    @media (max-width:480px){
-      max-width: 100vw;
-      width: 100%;
-    }  
+ 
     `
 
 export const Tema = styled.button`
@@ -43,18 +45,21 @@ border: none;
 border-radius: 8%;
 `
 export const Barra = styled.div`
-@media (mim-width:950px){
-position: sticky;
-top: 80px;
-  
+@media (max-width:768px) and (orientation:landscape){
+  display: flex;
+  align-items:center ;
+  justify-content: center;
 }
 `
 export const DivLogo = styled.img`
 
-margin-bottom: 8px;
-border-radius: 0px ;
+margin: 0 auto;
 width: ${(prop) => prop.tamanho ? prop.tamanho : "100px" };
 border-radius: none;
 border: 4 px black;
-
+display: block;
+float: center;
+@media (max-width:768px) and (orientation:landscape){
+  margin: 5px;
+}
 `
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Titulo } from "../Titulo"
-import { DivLogo, Tema, Img } from "./Styles" 
+import { DivLogo, Tema, Img, Barra,} from "./Styles" 
 import { Paragrafo } from "../Paragrfo"
 import Logo from "../image/hcj.png"
 import Logo1 from "../image/jquery.png"
@@ -9,16 +9,18 @@ import Logo2 from "../image/react.png"
 
 const Sidebar = props => (
     <>
+    
     <aside>
          <Img src="https://github.com/thiagoJCmds.png" alt="html" />
     </aside>
     <Titulo>Thiago</Titulo>
     <Paragrafo bold="bold">Desenvolvedor Front-End</Paragrafo>
     <Tema onClick={props.troca}>Tema</Tema>
+    <Barra>
     <DivLogo src={Logo}/>
     <DivLogo src={Logo2}/>
     <DivLogo tamanho='60px' src={Logo1}/>
-            
+    </Barra>
     </>
 )
 
